990b86f6cda157cb8271731c298ba9dd36571cf6
Ailurus-code <76646255+Ailurus-code@users.noreply.github.com>
2021-02-14 19:00:37 +0300

    Парсинг + перевод в обратную польскую запись.

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -4,40 +4,158 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define SIZE 1000
+#define NumberOfOperators 5
+
+int getOperatorPriority(const char c)
+{
+    switch (c)
+    {
+        case '^':
+            return 4;
+        case '*':
+        case '/':
+            return 3;
+        case '+':
+        case '-':
+            return 2;
+        case '=':
+            return 1;
+    }
+    return 0;
+}
+
+bool isOperator(char curChar)
+{
+    char Operators[NumberOfOperators] = {'+', '-', '*', '/', '^'};
+    for (int i = 0; i < NumberOfOperators; i++)
+    {
+        if (curChar == Operators[i]) return true;
+    }
+    return false;
+}
+
+char *makePostfixForm(char *inputStr, char *output)
+{
+    bool flag = false;
+    char outputStr[SIZE] = {0};
+    char operatorStack[SIZE] = {0};
+    int StackPointer = 0, StringPointer = 0;
+    for (int i = 0; i < strlen(inputStr); i++)
+    {
+        char cur = inputStr[i];
+        if (cur >= '0' && cur <= '9')
+        {
+            if (flag) outputStr[StringPointer] = cur, StringPointer++;
+            else
+            {
+                outputStr[StringPointer] = ' ';
+                StringPointer++;
+                outputStr[StringPointer] = cur;
+                StringPointer++;
+                flag = true;
+            }
+        }
+        else if (isOperator(cur))
+        {
+            flag = false;
+            while (StackPointer)
+            {
+                char temp = operatorStack[StackPointer-1];
+                if (isOperator(temp) && (getOperatorPriority(cur) <= getOperatorPriority(temp)))
+                {
+                    outputStr[StringPointer] = ' ';
+                    StringPointer++;
+                    outputStr[StringPointer] = temp;
+                    StringPointer++;
+                    StackPointer--;
+                }
+                else break;
+            }
+            operatorStack[StackPointer] = cur;
+            StackPointer++;
+        }
+        else if (cur == '(')
+        {
+            flag = false;
+            operatorStack[StackPointer] = cur;
+            StackPointer++;
+        }
+        else if (cur == ')')
+        {
+            flag = false;
+            bool flag = false;
+            while (StackPointer)
+            {
+                char temp = operatorStack[StackPointer-1];
+                if (temp == '(')
+                {
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                    outputStr[StringPointer] = ' ';
+                    StringPointer++;
+                    outputStr[StringPointer] = temp;
+                    StringPointer++;
+                    StackPointer--;
+                }
+            }
+            StackPointer--;
+        }
+    }
+    while (StackPointer)
+    {
+        outputStr[StringPointer] = ' ';
+        StringPointer++;
+        outputStr[StringPointer] = operatorStack[StackPointer-1];
+        StringPointer++;
+        StackPointer--;
+    }
+    strcpy(output, outputStr);
+    return output;
+}
+
 int calculatePolish(char inputStr[])
 {
-    int stack[1000];
+    int stack[SIZE];
     int sp = 0;
     for (int i = 0; i < strlen(inputStr); i++)
     {
         char c = inputStr[i];
         int x;
-        char number[128] = { 0 };
-        switch (c) {
-        case  ' ':
-        case '\n':
-            break;
-        case '+':
-            stack[sp - 2] = stack[sp - 2] + stack[sp - 1];  sp--;
-            break;
-        case '-':
-            stack[sp - 2] = stack[sp - 2] - stack[sp - 1];  sp--;
-            break;
-        case '*':
-            stack[sp - 2] = stack[sp - 1] * stack[sp - 2];  sp--;
-            break;
-        case '/':
-            stack[sp - 2] = stack[sp - 2] / stack[sp - 1];   sp--;
-            break;
-        default:
-            for (int j = i; inputStr[j] >= '0' && inputStr[j] <= '9'; j++)
-            {
-                number[j - i] = inputStr[j];
-            }
-            i += strlen(number);
-            x = atoi(number);
-            stack[sp] = x;
-            sp++;
+        char number[128] = {0};
+        switch (c)
+        {
+            case ' ':
+            case '\n':
+                break;
+            case '+':
+                stack[sp - 2] = stack[sp - 2] + stack[sp - 1];
+                sp--;
+                break;
+            case '-':
+                stack[sp - 2] = stack[sp - 2] - stack[sp - 1];
+                sp--;
+                break;
+            case '*':
+                stack[sp - 2] = stack[sp - 1] * stack[sp - 2];
+                sp--;
+                break;
+            case '/':
+                stack[sp - 2] = stack[sp - 2] / stack[sp - 1];
+                sp--;
+                break;
+            default:
+                for (int j = i; inputStr[j] >= '0' && inputStr[j] <= '9'; j++)
+                {
+                    number[j - i] = inputStr[j];
+                }
+                i += strlen(number);
+                x = atoi(number);
+                stack[sp] = x;
+                sp++;
         }
     }
     return stack[sp - 1];
@@ -46,5 +164,9 @@ int calculatePolish(char inputStr[])
 
 int main()
 {
-    printf("%d", calculatePolish("5 10 *"));
+    char *Expression = "23-16+45*2";
+    char check[SIZE] = {0};
+    char *res = makePostfixForm(Expression, check);
+    printf("%s\n%d", res, calculatePolish(res));
+    return 0;
 }
