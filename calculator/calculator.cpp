175df932fa1aeec4ef5d808e486bb6fb152ae4f2
tarloned <38500879+tarloned@users.noreply.github.com>
2021-02-14 14:35:45 +0300

    функция

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,4 +1,39 @@
-﻿int main()
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int calculatePolish(char* inputStr)
+{
+    int stack[1000];
+    int sp = 0;
+    while (!feof(stdin)) {
+        int c = getchar();
+        int x;
+        switch (c) {
+        case  ' ':
+        case '\n':
+            break;
+        case '+':
+            stack[sp - 2] = stack[sp - 2] + stack[sp - 1];  sp--;
+            break;
+        case '-':
+            stack[sp - 2] = stack[sp - 2] - stack[sp - 1];  sp--;
+            break;
+        case '*':
+            stack[sp - 2] = stack[sp - 1] * stack[sp - 2];  sp--;
+            break;
+        case '/':
+            stack[sp - 2] = stack[sp - 2] / stack[sp - 1];   sp--;
+            break;
+        default:
+            // получить число
+        }
+    }
+    return stack[sp - 1];
+}
+
+
+int main()
 {
     
 }
